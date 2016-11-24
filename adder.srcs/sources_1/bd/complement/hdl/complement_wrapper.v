7b5186555bd8c8e306fd04fb3cea257aeef41f91
Peter Lu <mcfloundinho@gmail.com>
2016-11-24 11:21:19 +0800

    complement: package ip

--- adder.srcs/sources_1/bd/complement/hdl/complement_wrapper.v
+++ adder.srcs/sources_1/bd/complement/hdl/complement_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2016.3 (win64) Build 1682563 Mon Oct 10 19:07:27 MDT 2016
-//Date        : Thu Nov 24 11:17:59 2016
+//Date        : Thu Nov 24 11:20:48 2016
 //Host        : DESKTOP-82NDHVB running 64-bit major release  (build 9200)
 //Command     : generate_target complement_wrapper.bd
 //Design      : complement_wrapper
